63decdae55e8db87371b6edeeef78e95d21f3f3b
dlkulp <damiankulp@gmail.com>
2015-05-23 15:23:27 -0700

    added materialize and other files and graham

---
+++ Plunder/platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,62 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/src/windows/NotificationProxy.js",
+        "id": "org.apache.cordova.dialogs.NotificationProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/src/windows/NetworkInfoProxy.js",
+        "id": "org.apache.cordova.network-information.NetworkInfoProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/windows8/SplashScreenProxy.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreenProxy",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.network-information": "0.2.15",
+    "org.apache.cordova.splashscreen": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
