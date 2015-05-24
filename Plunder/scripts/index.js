9c3414df26564bb2a331ec7f7c32d2febbe1ef03
dlkulp <damiankulp@gmail.com>
2015-05-24 02:43:00 -0700

    Added login and signup dialog, working on server stuff

--- Plunder/scripts/index.js
+++ Plunder/scripts/index.js
@@ -1,11 +1,12 @@
 ﻿$(function () {
+	// Plugins and stuff
 	$(".button-collapse").sideNav();
-	console.log("md5: " + MD5("hi"));
 	$('.main-car').slick();
+	$('.modal-trigger').leanModal({
+		"dismissible": false
+	});
 
-	function homeClick() {
-		$('.main-car').slick('slickGoTo', 0);
-	}
+	// Slide content when you click menu options
 	$('.deckButton').click(function () {
 		$('.main-car').slick('slickGoTo', 0);
 		console.log($('.main-car').slick('slickCurrentSlide'));
@@ -18,5 +19,33 @@
 	});
 	$('.optionsButton').click(function () {
 		$('.main-car').slick('slickGoTo', 3);
+	});
+
+	// Login
+
+
+	// Parse data (signup)
+	var d = new Date();
+	localStorage.clientKey = MD5(user.username + user.password + d.getTime());
+	$('#signupSubmit').click(function () {
+
+		var uname = toString($('#suname').val());
+		var pass = toString($('#spass').val());
+
+		// Validate new user (checks for unique username)
+
+
+		$('#modal1').closeModal();
+		// Send new (with empty deck)
+		var user = {
+			"username": uname,
+			"password": MD5(pass),
+			"key": localStorage.clientKey,
+			"deck": null
+		}
+
+		// Create New User Deck
+
+		// Send deck (if server allows)
 	});
 });
