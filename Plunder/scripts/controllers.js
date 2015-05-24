8f85f774290b9cdfcdcaf62c3101044290bab1e7
dlkulp <damiankulp@gmail.com>
2015-05-24 08:45:51 -0700

    probably last commit, cards and stuff show.  goodbye cruel WorlD....

--- Plunder/scripts/controllers.js
+++ Plunder/scripts/controllers.js
@@ -1,4 +1,13 @@
 ﻿var plunder = angular.module("plunder", []);
+var maybeUser = [];
 plunder.controller("plunderController", ["$scope", function($scope, $http) {
 	$scope.users = 5;
+	$scope.deck = [];
+	$scope.getDeck = function () {
+		$scope.deck = (typeof(sessionStorage.deck) !== "undefined") ? JSON.parse(sessionStorage.deck) : -1;
+	};
+	$scope.getUser = function () {
+		$http.get("dev.monochromicon.me/login.php")
+			.success(function (response) { maybeUser = response; });
+	}
 }]);
--- Plunder/scripts/index.js
+++ Plunder/scripts/index.js
@@ -5,6 +5,7 @@
 	$('.modal-trigger').leanModal({
 		"dismissible": false
 	});
+	$('.preloader-wrapper').hide();
 
 	// Slide content when you click menu options
 	$('.deckButton').click(function () {
@@ -21,8 +22,23 @@
 		$('.main-car').slick('slickGoTo', 3);
 	});
 
-	// Login
+	$('.logged').click(function () {
+		if ($(($('.logged'))[0]).text() == "Sign Out") {
+			sessionStorage = "undefined";
+		}
+	});
 
+	// Login
+	$("#loginSubmit").click(function () {
+		var uname = $("#luname").val().toString();
+		var pass = MD5($('#lpass').val().toString());
+
+		// Send to Server (DATABASE)
+		var signin = {
+			"username": uname,
+			"password": pass
+		}
+	});
 
 	// Parse Data (Signup)
 	$('#signupSubmit').click(function () {
@@ -46,8 +62,8 @@
 		var d = new Date();
 		sessionStorage.clientKey = MD5(uname + pass + d.getTime());
 
-		// Validate New User (checks for unique username)
-
+		// Validate New User (checks for unique username) (DATABASE)
+		
 
 		// Send New (with empty deck)
 		var user = {
@@ -57,9 +73,79 @@
 			"deck": deck
 		}
 
-		// Send User (if server allows)
+		// Send User (if server allows) (DATABASE)
 
 
+		// End
 		$('#modal1').closeModal();
+		$('.logged').html("Sign Out");
+	});
+
+	// Recolor svg's
+	$('#deck').click(function () {
+		if ($('.png').length > 0) {
+			var pngs = $('.png');
+			for (var x = 0; x < pngs.length; x++) {
+				pngID = $(pngs[x]).attr("id").split("-");
+				pngElement = pngID[0];
+				pngStrength = pngID[1];
+				if (pngElement == "water") {
+					if (pngStrength == 0)
+						$(pngs[x]).attr('src', 'images/png/water1.png');
+					else if (pngStrength == 1)
+						$(pngs[x]).attr('src', 'images/png/water2.png');
+					else if (pngStrength == 2)
+						$(pngs[x]).attr('src', 'images/png/water3.png');
+					else if (pngStrength == 3)
+						$(pngs[x]).attr('src', 'images/png/water4.png');
+					else if (pngStrength == 4)
+						$(pngs[x]).attr('src', 'images/png/water5.png');
+				} else if (pngElement == "fire") {
+					if (pngStrength == 0)
+						$(pngs[x]).attr('src', 'images/png/fire1.png');
+					else if (pngStrength == 1)
+						$(pngs[x]).attr('src', 'images/png/fire2.png');
+					else if (pngStrength == 2)
+						$(pngs[x]).attr('src', 'images/png/fire3.png');
+					else if (pngStrength == 3)
+						$(pngs[x]).attr('src', 'images/png/fire4.png');
+					else if (pngStrength == 4)
+						$(pngs[x]).attr('src', 'images/png/fire5.png');
+				} else if (pngElement == "earth") {
+					if (pngStrength == 0)
+						$(pngs[x]).attr('src', 'images/png/earth1.png');
+					else if (pngStrength == 1)
+						$(pngs[x]).attr('src', 'images/png/earth2.png');
+					else if (pngStrength == 2)
+						$(pngs[x]).attr('src', 'images/png/earth3.png');
+					else if (pngStrength == 3)
+						$(pngs[x]).attr('src', 'images/png/earth4.png');
+					else if (pngStrength == 4)
+						$(pngs[x]).attr('src', 'images/png/earth5.png');
+				} else if (pngElement == "air") {
+					if (pngStrength == 0)
+						$(pngs[x]).attr('src', 'images/png/air1.png');
+					else if (pngStrength == 1)
+						$(pngs[x]).attr('src', 'images/png/air2.png');
+					else if (pngStrength == 2)
+						$(pngs[x]).attr('src', 'images/png/air3.png');
+					else if (pngStrength == 3)
+						$(pngs[x]).attr('src', 'images/png/air4.png');
+					else if (pngStrength == 4)
+						$(pngs[x]).attr('src', 'images/png/air5.png');
+				} else if (pngElement == "electricity") {
+					if (pngStrength == 0)
+						$(pngs[x]).attr('src', 'images/png/elec1.png');
+					else if (pngStrength == 1)
+						$(pngs[x]).attr('src', 'images/png/elec2.png');
+					else if (pngStrength == 2)
+						$(pngs[x]).attr('src', 'images/png/elec3.png');
+					else if (pngStrength == 3)
+						$(pngs[x]).attr('src', 'images/png/elec4.png');
+					else if (pngStrength == 4)
+						$(pngs[x]).attr('src', 'images/png/elec5.png');
+				}
+			}
+		}
 	});
 });
